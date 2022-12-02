e79ccc178f4369e156d603a10a5f8bfbb924cc14
NDDDDDDDDD <102587802+NDDDDDDDDD@users.noreply.github.com>
2022-12-02 05:26:57 +0000

    Update trackerchecker.py

--- trackerchecker.py
+++ trackerchecker.py
@@ -4,7 +4,7 @@ import json
 from os import path
 import requests
 import socket
-
+down = []
 closed_trackers = []
 
 choice = input("1. TrackerChecker\n2. Add new tracker\nSelect mode: ")
@@ -28,19 +28,16 @@ if choice == "1":
                     async with session.get(url, ssl=False) as response:
                         obj = await response.read()
                         status_code = response.status
-                        if status_code > 500 or  "needs to review the security of your connection before proceeding" in str(obj):
-                            print(f"{name} is down!")
-                            print(f"error code: {status_code}")
+                        if status_code > 500 or "needs to review the security of your connection before proceeding" in str(obj):
+                            down.append(name)
                         elif "The backend is currently offline" in str(obj):
-                            print(f"{name} is down!")
-                            print(f"error code: {status_code}")
+                            down.append(name)
                         elif "down for maintenance" in str(obj):
-                            print(f"{name} is down!")
+                            down.append(name)
                         elif search_term in str(obj):
                             closed_trackers.append(name)
                         else:
                             print(f"{name} is open! {response.url}")
-                            print(status_code)
                 except asyncio.exceptions.TimeoutError:
                     print(f"{name} timed out!")
                     pass
@@ -68,6 +65,11 @@ if choice == "1":
     closed_trackers = closed_trackers.replace("'", "")
     closed_trackers = closed_trackers.replace("]", "")
     print(f"\nClosed trackers: {closed_trackers}")
+    down.sort()
+    down = str(down).replace("[", "")
+    down = down.replace("'", "")
+    down = down.replace("]", "")
+    print(f"Currently down: {down}")
     input("Done! Press any button to exit ")
 
 elif choice == "2":
