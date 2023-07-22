d3fea9870caf7003ac3a090389f5b9638bf3e53f
NDDDDDDDDD <102587802+NDDDDDDDDD@users.noreply.github.com>
2023-07-22 11:42:31 +0000

    Update trackerchecker.py

--- trackerchecker.py
+++ trackerchecker.py
@@ -1,78 +1,80 @@
 import asyncio
 import aiohttp
 import json
-import requests
 
-error_msg = ["needs to review the security of your connection before proceeding", "Open registration is currently disabled", "bing.com", "We do not allow members to register multiple accounts with the same IP Address", "Nebulance is temporarily offline for planned maintenance. Leave your torrents seeding in your client and we will be back shortly.", "Retrying, please wait", "404 Not Found", "down for maintenance", "The backend is currently offline","and the server is refusing to fulfill it", "</h3></td></tr></table></body></html>"]
-down = []
-temp_down = []
-closed_trackers = []
+error_msg = [
+    "needs to review the security of your connection before proceeding",
+    "Open registration is currently disabled",
+    "bing.com",
+    "We do not allow members to register multiple accounts with the same IP Address",
+    "Nebulance is temporarily offline for planned maintenance. Leave your torrents seeding in your client and we will be back shortly.",
+    "Retrying, please wait",
+    "404 Not Found",
+    "down for maintenance",
+    "The backend is currently offline",
+    "and the server is refusing to fulfill it",
+    "</h3></td></tr></table></body></html>",
+]
+closed_url_msg = [
+    "https://redbits.xyz/login",
+    "https://torrentseeds.org/buyinvite",
+    "https://pretome.info/index.php?page=home&msg=not_qualified",
+]
 
-r = requests.get("https://raw.githubusercontent.com/NDDDDDDDDD/TrackerChecker/main/trackers.json")
-trackers = r.text
-URLS = json.loads(trackers)
-conn = aiohttp.TCPConnector(limit_per_host=100, limit=100)
-PARALLEL_REQUESTS = len(URLS)
-async def gather_with_concurrency(n):
+async def fetch_trackers():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://raw.githubusercontent.com/NDDDDDDDDD/TrackerChecker/main/trackers.json") as response:
+            trackers = await response.text()
+    return json.loads(trackers)
+
+async def gather_with_concurrency(n, urls):
+    conn = aiohttp.TCPConnector(limit_per_host=100, limit=100)
     timeout = aiohttp.ClientTimeout(total=30)
     semaphore = asyncio.Semaphore(n)
-    session = aiohttp.ClientSession(connector=conn, timeout=timeout)
-    print("Starting...")
+    async with aiohttp.ClientSession(connector=conn, timeout=timeout) as session:
+        print("Starting...")
+        tasks = [get(url.get("url"), url.get("name"), url.get("search_term"), semaphore, session) for url in urls]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        return results
 
-    async def get(url, name, search_term):
-        async with semaphore:
-            try:
-                async with session.get(url, ssl=False) as response:
-                    obj = await response.read()
-                    for error in error_msg:
-                        if error in str(obj):
-                            temp_down.append(name)
-                    if response.status >= 500 or name in temp_down:
-                        down.append(name)
-                    elif search_term in str(obj):
-                        closed_trackers.append(name)
-                    elif "https://redbits.xyz/login" in str(response.url):
-                        closed_trackers.append(name)
-                    elif "https://torrentseeds.org/buyinvite" in str(response.url):
-                        closed_trackers.append(name)
-                    elif "https://pretome.info/index.php?page=home&msg=not_qualified" in str(response.url):
-                        closed_trackers.append(name)
-                    else:
-                        print(f"{name} is open! {response.url}")
-            except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ServerDisconnectedError,
-                    aiohttp.client_exceptions.ClientConnectorError) as error:
-                down.append(name)
-                pass
+async def get(url, name, search_term, semaphore, session):
+    temp_down = []
+    down = []
+    closed_trackers = []
+    async with semaphore:
+        try:
+            async with session.get(url, ssl=False) as response:
+                obj = await response.read()
+                for error in error_msg:
+                    if error in str(obj):
+                        temp_down.append(name)
+                if response.status >= 500 or name in temp_down:
+                    down.append(name)
+                elif search_term in str(obj):
+                    closed_trackers.append(name)
+                elif str(response.url) in closed_url_msg:
+                    closed_trackers.append(name)
+                else:
+                    print(f"{name} is open! {response.url}")
+        except Exception as error:
+            down.append(name)
+            print(f"Error for {name}: {str(error)}")
+    return down, closed_trackers
 
-    await asyncio.gather(
-        *(
-            get(
-                url.get("url"),
-                url.get("name"),
-                url.get("search_term"),
-            )
-            for url in URLS
-        )
-    )
-    await session.close()
-
+def print_results(results):
+    down = []
+    closed = []
+    for result in results:
+        if isinstance(result, Exception):
+            print(f"Error: {str(result)}")
+        else:
+            down.extend(result[0])
+            closed.extend(result[1])
+    print("\nCurrently down: ", ", ".join(sorted(down)))
+    print("\nCurrently closed: ", ", ".join(sorted(closed)))
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(gather_with_concurrency(PARALLEL_REQUESTS))
-    conn.close()
-
-
-def sortt(status_):
-    status_.sort()
-    status_str = ", ".join(status_)
-    if down == status_:
-        print(f"\nCurrently down: {status_str}")
-    else:
-        print(f"\nCurrently closed: {status_str}")
-
-
-sortt(down)
-sortt(closed_trackers)
-
-input("Done! Press enter to exit ")
+    trackers = asyncio.run(fetch_trackers())
+    results = asyncio.run(gather_with_concurrency(len(trackers), trackers))
+    print_results(results)
+    input("Done! Press enter to exit ")
